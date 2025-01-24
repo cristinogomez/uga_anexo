db9dea3fcfbd501eb62ae56bafde7363992567ec
cristinogomez <133212525+cristinogomez@users.noreply.github.com>
2025-01-24 10:03:24 +0100

    Update cot.py

--- cot.py
+++ cot.py
@@ -74,7 +74,7 @@ def cot():
         fechaTarget = dt.date.today()+dt.timedelta (days = 7)
 
         df_filtrado=df[df['Fecha']<= str(fechaTarget)]
-        df_bloqueosbymedico=df.groupby(['Medico']).sum()
+        df_bloqueosbymedico=df.groupby(['Medico'])['Bloqueos'].sum()
         st.text('Huecos bloquedos que se deben citar:')
         with st.container(border=True):
             st.dataframe(df_filtrado,hide_index=True)
