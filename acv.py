c54211423ecd181072819aa8d5a9daab86932e1c
cristinogomez <133212525+cristinogomez@users.noreply.github.com>
2025-02-14 08:15:08 +0100

    Update acv.py

--- acv.py
+++ acv.py
@@ -1,20 +1,24 @@
 import streamlit as st
+import datetime as dt
+from datetime import datetime
 import pandas as pd
 
+def getdata():
+      df=pd.read_csv("ACV.csv",encoding='utf-8',parse_dates=['Fecha'])
+      return df
 def acv():    
     st.subheader("Bloqueos Agendas ACV")
-
-
-    df = pd.read_csv("ACV.csv",encoding='utf-8',parse_dates=['Fecha'])
-    
-    df['Fecha']= pd.to_datetime(df['Fecha'])
+    df = getdata()
+    #df['Fecha']= pd.to_datetime(df['Fecha'])
     col3,col4 = st.columns([2, 1])
     with col3:
         med=st.selectbox('Medico',['Requejo','Fernandez','Autogestion','Zafra','Fariñas','Martinez Leon','Mondragon'])
-        df_filtrado_medico = df[df['Medico']== med].head()
-        
+        df_filtrado_medico = df[df['Medico']== med]
         with st.container(border=True):
-            st.dataframe(df_filtrado_medico,hide_index=True,use_container_width=800)
+            dff=st.dataframe(df_filtrado_medico,selection_mode = "single-row"
+                        ,column_config={
+                                    "Fecha": st.column_config.DateColumn("Fecha del bloqueo",format="DD/MM/YYYY")}
+                        ,hide_index=True,use_container_width=800)
     with col4:
         st.subheader("Datos")
         with st.container(border=True):
@@ -24,17 +28,12 @@ def acv():
 
     
     with col1:
-
-        #df = pd.read_csv("ACV.csv",encoding='utf-8', parse_dates=['Fecha'])
-        df['Fecha']= pd.to_datetime(df['Fecha'])
-        df_mask = df["Bloqueos"] >0
-        filtered_df = df[df_mask]
-
-        with st.form("my_form"):
-                        edited_df = st.data_editor(filtered_df,
-                                                width=800,
-                                                height=540,
-                                                column_config={
+        with st.form('my_form1'):
+            #df['Fecha']= pd.to_datetime(df['Fecha'])
+            edited_df = st.data_editor(df,
+                                        width=800,
+                                        height=540,
+                                        column_config={
                                                         "Fecha": st.column_config.DateColumn("Fecha del bloqueo",format="DD/MM/YYYY"),
                                                         "Medico": st.column_config.SelectboxColumn(
                                                         "Facultativo",
@@ -55,18 +54,13 @@ def acv():
                                                                                                 step=1)},
                                         num_rows='dynamic',use_container_width=True)          
             
-                        boton_guardar=st.form_submit_button('Save')
+            boton_guardar=st.form_submit_button('Save')
 
 
         if boton_guardar:
-                df2 = pd.DataFrame(edited_df)
-        
-                df2.to_csv('ACV.csv',index=False)
-
-                mensaje=st.success('Los datos se han guardado corecctamente')
-
-                        
-
+            edited_df.to_csv('ACV.csv',index=False)
+            mensaje=st.success('Los datos se han guardado corecctamente')
+            st.rerun()
     with col2:
         st.subheader("Notificaciones")
 
@@ -74,5 +68,3 @@ def acv():
 
         medico=(df["Medico"].iloc[0])
         st.info(f"Total número de citas bloqueados: {huecos}. El primer hueco bloquedo corresponde a: {medico}")
-
-
